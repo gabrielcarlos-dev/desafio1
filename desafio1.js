9c36a9412d538ac689f52f019300bfb22207bb15
gabrielcarlos-dev <gabrielcarlos200211@gmail.com>
2025-11-30 10:11:53 -0300

    voltando de novo

---
+++ desafio1.js
@@ -0,0 +1,35 @@
+const nomeDoHeroi = "Pac";
+let xpHeroi = "Digite a quantidade de XP do herói:";
+let elo;
+
+switch (true) {
+    case xpHeroi >= 0 && xpHeroi <= 1000:
+        elo = "Bronze";
+        break;
+    case xpHeroi > 1001 && xpHeroi <= 2000:
+        elo = "Prata";
+        break;
+    case xpHeroi > 2001 && xpHeroi <= 5000:
+        elo = "Ouro";
+        break;
+    case xpHeroi > 6001 && xpHeroi <= 7000:
+        elo = "Platina";
+        break;
+    case xpHeroi > 7001 && xpHeroi <= 8000:
+        elo = "Diamante";
+        break;
+    case xpHeroi > 8001 && xpHeroi <= 9000:
+        elo = "Mestre";
+        break;
+    case xpHeroi > 9001 && xpHeroi <= 10000:
+        elo = "Grão-Mestre";
+        break;
+    case xpHeroi >= 10000:
+        elo = "Lendário";
+        break;
+    default:
+        elo = "XP inválido";
+        break;
+}
+
+console.log(`O herói ${nomeDoHeroi} possui ${xpHeroi} de XP e seu elo é ${elo}.`);
